56dc3de20b080a7dbc752f98790e81305c3e9643
vanstee <vanstee@highgroove.com>
2011-11-18 01:01:29 -0500

    [#21194207] Make sure we chown /usr/local for homebrew while we're still sudo

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
-echo "Installing GCC"
+echo "install gcc"
 gcc="GCC-10.7-v2.pkg"
 curl -L https://github.com/downloads/kennethreitz/osx-gcc-installer/$gcc -o /tmp/$gcc
 /usr/sbin/installer -pkg /tmp/$gcc -target /
 
-echo "Installing Chef"
+echo "install chef"
 gem install rdoc chef ohai --no-ri --no-rdoc --source http://gems.opscode.com --source http://gems.rubyforge.org
 mkdir -p /tmp/chef
 curl -L https://raw.github.com/highgroove/bootstrap/solo.rb -o /tmp/chef/solo.rb
 
-echo "Running Chef"
+echo "prepare for homebrew install"
+sudo chown -R `whoami`:staff /usr/local
+
+echo "run chef-solo"
 chef-solo -c /tmp/chef/solo.rb
