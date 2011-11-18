773158e6c60d8dab9fdf292f0a2f4ff525d763d3
vanstee <vanstee@highgroove.com>
2011-11-18 01:11:54 -0500

    Oops start at version 0.1.0

--- cookbooks/homebrew/metadata.rb
+++ cookbooks/homebrew/metadata.rb
@@ -1,5 +1,5 @@
 recipe           "homebrew", "Install Homebrew"
-version          "0.3.5"
+version          "0.1.0"
 description      "Install Homebrew"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 license          "Apache 2.0"
