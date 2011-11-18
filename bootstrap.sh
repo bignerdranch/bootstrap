4e4bd5c4f043918a0bcdfeb74f9dd38a0f58d448
vanstee <vanstee@highgroove.com>
2011-11-18 00:03:28 -0500

    [#21182311] Install dependencies and chef

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,11 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+echo "Installing GCC"
+gcc="GCC-10.7-v2.pkg"
+curl -L https://github.com/downloads/kennethreitz/osx-gcc-installer/$gcc -o /tmp/$gcc
+/usr/sbin/installer -pkg /tmp/$gcc -target /
+
+echo "Installing Chef"
+gem install rdoc chef ohai --no-ri --no-rdoc --source http://gems.opscode.com --source http://gems.rubyforge.org
+mkdir -p /tmp/chef
+curl -L https://raw.github.com/highgroove/bootstrap/solo.rb -o /tmp/chef/solo.rb
