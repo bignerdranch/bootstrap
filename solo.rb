46767524aa8e393f2e8fe205844e383b9fdd18d9
vanstee <vanstee@highgroove.com>
2011-11-19 15:32:53 -0500

    Change location of local cookbooks

--- solo.rb
+++ solo.rb
@@ -1,5 +1,5 @@
 json_attribs    File.join(File.dirname(__FILE__), 'chef.json')
-cookbook_path   File.join(File.dirname(__FILE__), '..', 'cookbooks')
+cookbook_path   File.join(File.dirname(__FILE__), 'cookbooks')
 file_store_path File.join(File.dirname(__FILE__), '..')
 file_cache_path File.join(File.dirname(__FILE__), '..')
 log_level :debug
