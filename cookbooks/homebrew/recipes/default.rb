350942244604d5d411041185b357d58417f72753
vanstee <vanstee@highgroove.com>
2011-11-19 15:37:08 -0500

    Install homebrew as the current user

--- cookbooks/homebrew/recipes/default.rb
+++ cookbooks/homebrew/recipes/default.rb
@@ -1,4 +1,6 @@
 execute "install homebrew" do
   command '/usr/bin/ruby -e "$(curl -fsSL https://raw.github.com/gist/323731)"'
+  user ENV['SUDO_USER']
+  group "staff"
   not_if 'test -e /usr/local/bin/brew'
 end
