30fa3abc43247b998472c3ddf9f087110180a364
vanstee <vanstee@highgroove.com>
2011-12-29 16:21:23 -0500

    [#21988735] Grep for the postgres plist file to use - thanks @wfarr

--- cookbooks/postgresql/recipes/default.rb
+++ cookbooks/postgresql/recipes/default.rb
@@ -6,8 +6,8 @@ execute 'launch postgresql on startup' do
   command <<-EOF
     initdb /usr/local/var/postgres
     mkdir -p ~/Library/LaunchAgents
-    version=`ls /usr/local/Cellar/postgresql/ | sort -n | tail -1`
-    cp /usr/local/Cellar/postgresql/$version/org.postgresql.postgres.plist ~/Library/LaunchAgents/
+    plist=`brew list postgres | grep org.postgresql.postgres.plist`
+    cp $plist ~/Library/LaunchAgents/
     launchctl load -w ~/Library/LaunchAgents/org.postgresql.postgres.plist
   EOF
   not_if 'test -e ~/Library/LaunchAgents/org.postgresql.postgres.plist'
